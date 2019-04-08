1b8f5edb7759e4bd6a2bb17e965dab4cddda530e
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-08 14:44:31 +0600

    Добавьте файлы проекта.

---
+++ Practice_4/Money.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    public class Money 
+    {
+        public int ban { get; set; }
+        public int cnt { get; set; }
+        public int sum { get; set; }
+
+        public Money()
+        {
+            ban = 10;
+            cnt = 1;
+            sum = ban * cnt;
+        }
+        public Money(int value, int counts) 
+        {
+            ban = value;
+            cnt = counts;
+            sum = value * counts;
+        }
+
+        public void ShowAll() // Вывести номинал и количество купюр
+        {
+            Console.WriteLine("Value - {0}, counts - {1}", ban, cnt);
+        }
+
+        public bool CostEstimation(int productPrice) //Определить, хватит ли денежных средств на покупку товара на сумму N рублей
+        {
+            if (sum > productPrice)
+            {
+                Console.WriteLine("You have enough money to buy this product");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough money to buy this product");
+                return false;
+            }
+        }
+
+        public int GetNumberOfPieces(int productPrice) //Определить, сколько штук товара стоимости n рублей можно купить на имеющиеся денежные
+        {
+            if (productPrice != 0)
+            {
+                return sum / productPrice;
+            }
+            else
+            {
+                Console.WriteLine("Wrong product price");
+                return 0;
+            }
+        }
+
+        public int BanknotesValue
+        {
+            get { return ban; }
+            set { ban = value; }
+        }
+
+        public int BanknotesCounts
+        {
+            get { return cnt; }
+            set { cnt = value; }
+        }
+
+        public int Sum
+        {
+            get { return sum; }
+        }
+    }
+}
